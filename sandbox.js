ce358447cb3921a2a3c65cc82e4c487c670f9ac2
Punchline <re70977za@outlook.com>
2022-09-18 20:27:35 +0430

    adding fetch API

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,31 @@
+// fetching data using The fetch API
+
+function MyFetch(){
+    fetch('Thejsons/A.json').then((response) => {
+        console.log('resolved',response)
+        return response.json()
+    }).then(data => {
+        console.log(data)
+        return fetch('Thejsons/B.json')
+    }).then((response) => {
+        console.log('resolved',response)
+        return response.json()
+    }).then(data => {
+        console.log(data)
+        return fetch('Thejsons/C.json')
+    }).then((response) => {
+        console.log('resolved',response)
+        return response.json()
+    }).then(data => {
+        console.log(data)
+    }).catch(err => {
+        console.log('rejected')
+    })
+}
+
+// fetching data using XML manually
+console.log("-----------------------fetching data using XML manually")
+
 const getTodos = (resource) => {
 
     return new Promise((resolve,reject) => {
@@ -28,7 +56,12 @@ getTodos('Thejsons/A.json').then(data =>{
     return getTodos('Thejsons/C.json')
 }).then(data => {
     console.log('promise 3 resolved : ',data)
-    
+    // fetching data using The fetch API
+    console.log("-----------------------fetching data using The fetch API")
+    MyFetch()
 }).catch(err => {
     console.log('promise rejected : ', err)
+    // fetching data using The fetch API
+    console.log("-----------------------fetching data using The fetch API")
+    MyFetch
 })
